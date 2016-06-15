d3c2f199004ab54753d1195eed7dbad49c6ea57b
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2016-06-15 12:53:25 -0400

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,12 +18,12 @@ function showQuestion(questionId) {
 
 function timer(){     		
 	countDown -- ; 
-console.log(countDown);
+// console.log(countDown);
 	$('#display').html(countDown);
 if(countDown === 0){
     clearInterval(intervalID);
     questionId++
-    triviaQuestions(questions[questionCounter]);
+    // triviaQuestions(questions[questionCounter]);
 }
 }
 
@@ -33,30 +33,28 @@ $('.triviaBox').hide();
   $('.intro').hide(); 
   $('.triviaBox').show();
   $('.display').html(questions.timer);
-  triviaQuestions(questions[questionCounter]);
+   triviaQuestions(questions[questionCounter]);
   
 
 });
  
 
 var questions = [{
-    question : 'Who played Danny Tanner in the TV series "Full House"?',
-    choices :["Bob Saget","Dave Coulier","John Posey"],
-    answer : "Bob Saget",
-    img : '1fullHouse.png'
+    question: 'Who played Danny Tanner in the TV series "Full House"?',
+    choices:["Bob Saget","Dave Coulier","John Posey"],
+    answer: "Bob Saget",
+    img: '1fullHouse.png'
 }, {
 	question:  'Which "Wonder Years" favorite was only supposed to appear in the pilot?',
-    choices :["Winnie Cooper","Paul Pfeiffer","Wayne Arnold"],
-    answer : "Winnie Cooper",
-    img : '2wonderYears.png'
+    choices:["Winnie Cooper","Paul Pfeiffer","Wayne Arnold"],
+    answer: "Winnie Cooper",
+    img: '2wonderYears.png'
 }, {
-
-    question : 'Which 2016 presidential candidate made a cameo on "The Fresh Prince of Bel-Air"?',
-    choices :["Hillary Clinton","Jeb Bush","Donald Trump"],
-    answer : "Donald Trump",
-    img : '1fullHouse.png'
+    question: 'Which 2016 presidential candidate made a cameo on "The Fresh Prince of Bel-Air"?',
+    choices:["Hillary Clinton","Jeb Bush","Donald Trump"],
+    answer: "Donald Trump",
+    img: 'freshPrince.png'
 },  {
-  
     question: 'Which 90\'s teen queen turned down the lead role on "Buffy the Vampire Slayer"?',
     choices:["Keri Russell","Katie Holmes","Jennifer Love Hewitt"],
     answer: "Katie Holmes",
@@ -68,29 +66,27 @@ var questions = [{
     answer: "Mrs. Edna Garrett",
     img: 'factsLife.png'
 },  {
-
     question:  'What was the name of Arnold\'s fish on Different Strokes?',
     choices:["Goldie","Abraham","Ted"],
     answer: "Abraham",
     img: 'diffStrokes.png'
 },  {
-
-    question:  'What was the name of the bar/restaurant on Three\'s Company?',
+    question:'What was the name of the bar/restaurant on Three\'s Company?',
     choices:["Bar None","Drunken Monkey","Regal Beagle"],
     answer: "threesCompany",
+    img: 'threesCompany.png'
 },  {
-
     question:  'Where was Will "born and raised?',
     choices:["New York","Vermont","West Philadelphia"],
     answer: "West Philadelphia",
     img: 'freshPrince.png'
-},  {
 }];
 
 
 
 
 var triviaQuestions = function(obj) {	
+	console.log(obj);
     countDown =30;
         clearInterval(intervalID);
          intervalID = setInterval(timer,1000);
@@ -99,43 +95,34 @@ var triviaQuestions = function(obj) {
   	$('.tv').replaceWith('<img class="tv" src="assets/images/' + obj.img + '">');
 
     for (var i = 0; i < obj.choices.length; i++) {
-    	$('#choice' + (i + 1)).html(obj.choices[i]);
+    	$('#choice' + (i + 1)).html(obj.choices[i]).data("questionId", i);
   		
   	};
-  	questionCounter ++;
+  	// questionCounter ++;
  
-	$(".choices" ).click(function() {
-  		console.log('hey look here for this' + this.innerHTML);
-  		console.log('also look here for that' + obj.answer)
-
-  		if (this.innerHTML.trim() == (obj.answer.trim())){
-  				console.log('thing fired this is true');
-  				// $('#question').replaceWith(obj.question);
-  				console.log(obj.answer);
-  				console.log('you r teh winnerz');
-  				triviaQuestions(questions[questionCounter]);
-  		} else { triviaQuestions(questions[questionCounter]);
-  				wrong++
-
-  		}			
-	})
-
+	
 }
 
- // function displayNext() {
- //    triviaBox.fadeOut(function() {
- //      $('#question').remove();
-      
- //      if(questionCounter < questions.length){
- //        var nextQuestion = createQuestionElement(questionCounter);
- //        triviaBox.append(nextQuestion).fadeIn();
- //        if (!(isNaN(selections[questionCounter]))) {
- //          $('input[value='+ selections[questionCounter]+']').prop('checked', true);
- //        }
-        
-  //if correct answer is clicked then move to the next question and add 1 to right
-  //add one to wrong
-  
+$(document).on("click", ".choices", function() {
+	var index = $(this).data("questionId");
+	var obj = questions[index];
+	console.log('hey look here for this' + this.innerHTML);
+	console.log('also look here for that' + obj.answer)
+
+	if (this.innerHTML.trim() == (obj.answer.trim())){
+		console.log('thing fired this is true');
+		// $('#question').replaceWith(obj.question);
+		console.log(obj.answer);
+		console.log('you r teh winnerz');
+		questionCounter ++;
+		triviaQuestions(questions[questionCounter]);
+	} else { 
+		questionCounter ++;
+		triviaQuestions(questions[questionCounter]);
+		wrong++
+
+	}			
+})
 
 
 
